1085d59b42aa3558a61025dda7a0e256e74ebf35
Kazuma Fujimaki <fujimaki-k@fujimakishouten.com>
2018-06-23 13:13:42 +0900

    Sanitize を SanitizeService に変更し、テストでは @Autowired を利用するようにした

--- src/main/kotlin/com/fujimakishouten/homework/service/SanitizeService.kt
+++ src/main/kotlin/com/fujimakishouten/homework/service/SanitizeService.kt
@@ -1,8 +1,10 @@
 package com.fujimakishouten.homework.service
 
+import org.springframework.stereotype.Service
 import java.text.Normalizer
 
-class Sanitize {
+@Service
+class SanitizeService {
     /**
      * 渡された文字列を正規化する
      *
--- src/test/kotlin/com/fujimakishouten/homework/service/SanitizeServiceTests.kt
+++ src/test/kotlin/com/fujimakishouten/homework/service/SanitizeServiceTests.kt
@@ -3,16 +3,18 @@ package com.fujimakishouten.homework.service
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.junit4.SpringRunner
 
 @RunWith(SpringRunner::class)
 @SpringBootTest
-class HomeworkApplicationTests {
+class SanitizeServiceTests {
+    @Autowired
+    lateinit var sanitize: SanitizeService
 
     @Test
     fun ShouldNormalizeText() {
-        val sanitize = Sanitize()
         val string = "    \u202b１２３\r\nＡＢＣ\rｄｅｆ\nｱｲｳｴｵｶﾞ    "
         val result = "123\nABC\ndef\nアイウエオガ"
 
