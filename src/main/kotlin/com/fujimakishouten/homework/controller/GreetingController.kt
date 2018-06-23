8747722f5bfcb94f26bc8f3a0056ccff12f28087
Kazuma Fujimaki <fujimaki-k@fujimakishouten.com>
2018-06-24 00:27:25 +0900

    画面を表示するのに  Model ではなく ModelAndView を使うようにした

--- src/main/kotlin/com/fujimakishouten/homework/controller/GreetingController.kt
+++ src/main/kotlin/com/fujimakishouten/homework/controller/GreetingController.kt
@@ -5,14 +5,17 @@ import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.servlet.ModelAndView
 
 @Controller
 class GreetingController {
     @GetMapping("/hello", "/hello/{name}")
-    fun hello(model: Model, sanitize: SanitizeService, @PathVariable("name", required = false) name: String?): String {
+    fun hello(mav: ModelAndView, sanitize: SanitizeService, @PathVariable("name", required = false) name: String?): ModelAndView {
         val message = "Hello, %s.".format(sanitize.normalize(name?: "world"))
-        model.addAttribute("message", message)
 
-        return "hello/index"
+        mav.addObject("message", message)
+        mav.viewName = "hello/index"
+
+        return mav
     }
 }
